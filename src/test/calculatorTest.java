a33e515c7a4f725ddb499efa9d855e740fa1e86d
yanglei2020 <760236741@qq.com>
2020-07-31 10:38:57 +0800

    第3次提交,使用Before和After

--- src/test/calculatorTest.java
+++ src/test/calculatorTest.java
@@ -1,10 +1,28 @@
 package test;
 
 import Demo01.calculator;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class calculatorTest {
+
+    /**
+     * 初始化
+     */
+    @Before
+    public void init(){
+        System.out.println("init......");
+    }
+
+    /**
+     * 关闭执行资源
+     */
+    @After
+    public void close(){
+        System.out.println("close......");
+    }
     /*
     测试add方法
      */
