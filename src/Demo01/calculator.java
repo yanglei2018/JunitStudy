80c00ada0d0049e3c71aa5713d85a1d3fd73dbb4
yanglei2020 <760236741@qq.com>
2020-07-31 09:46:25 +0800

    第一次提交

---
+++ src/Demo01/calculator.java
@@ -0,0 +1,23 @@
+package Demo01;
+
+public class calculator {
+    /**
+     * 加法
+     * @param a
+     * @param b
+     * @return
+     */
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    /**
+     * 减法
+     * @param a
+     * @param b
+     * @return
+     */
+    public int sub(int a, int b){
+        return a+b;
+    }
+}
